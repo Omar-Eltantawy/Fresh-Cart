e3b691607a81d46feaa61559d83f09dfe564d9e8
Your Name <you@example.com>
2025-07-09 20:47:23 +0300

    Initial deploy

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -29,7 +29,7 @@ const CartContextProvider = ({ children }) => {
 
  const payOnlineAPI = ({ cartId, shippingAddress }) =>
   axiosInstance.post(
-    `/orders/checkout-session/${cartId}?url=fresh-cart-theta-nine.vercel.app`,
+    `/orders/checkout-session/${cartId}?url=fresh-cart-ecommerce-app.vercel.app`,
     { shippingAddress }
   );
 
