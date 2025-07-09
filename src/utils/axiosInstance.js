cdc3a8e7310358f399c4aff5a78deea0831a90a4
Your Name <you@example.com>
2025-07-09 20:56:34 +0300

    some changes

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const token = localStorage.getItem("token");
 
 export const axiosInstance = axios.create({
-    baseURL:import.meta.env.VITE_BASE_API_URL,
+    baseURL:"https://ecommerce.routemisr.com/api/v1",
     headers:{
         token: token || ""
     }
